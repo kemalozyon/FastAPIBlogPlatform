e07a7e4810bf21dcdc86e4deaff42987576dc24e
Kemal Özyön <kemalozyon51@gmail.com>
2026-05-28 16:45:29 +0300

    database logic added

---
+++ database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+## Changing the url is the only thing that we need to change when we need to change our db
+SQLALHEMY_DATABASE_URL = "sqlite:///./blog.db"
+
+engine = create_engine(
+    SQLALHEMY_DATABASE_URL,
+    connect_args={"check_same_thread" : False}
+)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    with SessionLocal() as db:
+        yield db
